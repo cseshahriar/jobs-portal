8ee59cf0eeff8bc67f43ef868255623c110053ea
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-03-04 08:18:55 +0600

    job create, update, delete api

--- backend/job/urls.py
+++ backend/job/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('jobs', views.job_list, name='job-list'),
-    path('<str:pk>/jobs', views.job_detail, name='job-detail'),
+    path('', views.job_list, name='job_list'),
+    path('<str:pk>/detail', views.job_detail, name='job_detail'),
+    path('create/', views.job_create, name='job_create'),
+    path('<str:pk>/update/', views.job_update, name='job_update'),
+    path('<str:pk>/delete/', views.job_delete, name='job_delete'),
 ]
--- backend/job/views.py
+++ backend/job/views.py
@@ -1,9 +1,9 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 from .serializers import JobSerializer
 from .models import Job
-
 
 @api_view(['GET'])
 def job_list(request):
@@ -17,3 +17,30 @@ def job_detail(request, pk):
     job = get_object_or_404(Job, id=pk)
     serializer = JobSerializer(job, many=False)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def job_create(request):
+    serializer = JobSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def job_update(request, pk):
+    job = get_object_or_404(Job, id=pk)
+    serializer = JobSerializer(instance=job, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def job_delete(request, pk):
+    job = get_object_or_404(Job, id=pk)
+    job.delete()
+    return Response({
+        'message': 'Job deleted successfully'},
+        status=status.HTTP_204_NO_CONTENT
+    )
