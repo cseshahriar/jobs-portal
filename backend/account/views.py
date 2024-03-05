57aedd00db3d0f0bdd70e5426f0adf5c1034b2d4
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-03-05 08:30:33 +0600

    user update api

--- backend/account/views.py
+++ backend/account/views.py
@@ -42,3 +42,21 @@ def register(request):
 def current_user(request):
     serializer = UserSerializer(request.user)
     return Response(serializer.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_user(request):
+    user = request.user
+    data = request.data
+    user.first_name = data['first_name']
+    user.last_name = data['last_name']
+    user.username = data['email']
+    user.email = data['email']
+
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+
+    user.save()
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data, status=status.HTTP_200_OK)
