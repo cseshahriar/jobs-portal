2594637ba9229c73b1deebedad94c326d4f72bfc
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-03-07 00:18:54 +0600

    home page and layout

---
+++ frontend/components/layout/Search.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Image from "next/image";
+
+const Search = () => {
+  return (
+    <div className="modalMask">
+      <div className="modalWrapper">
+        <div className="left">
+          <div style={{ width: "100%", height: "100%", position: "relative" }}>
+            <Image src="/images/job-search.svg" alt="search" />
+          </div>
+        </div>
+        
+        <div className="right">
+          <div className="rightContentWrapper">
+            <div className="headerWrapper">
+              <h2> SEARCH</h2>
+            </div>
+            <form className="form">
+              <div className="inputWrapper">
+                <div className="inputBox">
+                  <i aria-hidden className="fas fa-search"></i>
+                  <input
+                    type="text"
+                    placeholder="Enter Your Keyword"
+                    required
+                  />
+                </div>
+                <div className="inputBox">
+                  <i aria-hidden className="fas fa-industry"></i>
+                  <input
+                    type="text"
+                    placeholder="Enter City, State ..."
+                    required
+                  />
+                </div>
+              </div>
+              <div className="searchButtonWrapper">
+                <button type="submit" className="searchButton">
+                  Search
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
