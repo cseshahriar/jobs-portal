34a6ba9834a9ad244d58e5f695fc9a2460c6c945
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-05-14 22:26:20 +0600

    display user data in header

--- frontend/components/layout/Header.js
+++ frontend/components/layout/Header.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+import AuthContext from '../../context/AuthContext';
+
 
 const Header = () => {
+  const {loading, user} = useContext(AuthContext);
+
   return (
     <div className="navWrapper">
       <div className="navContainer">
@@ -23,11 +27,45 @@ const Header = () => {
             </button>
           </Link>
 
-          <Link href="/login">
-            <button className="loginButtonHeader">
-              <span>Login</span>
-            </button>
-          </Link>
+          { user ? (
+            <div className="btn dropdown ml-3">
+              <a 
+                className="btn dropdown-toggle mr-4"
+                id="dropDownMenuButton"
+                data-toggle="dropdown"
+                arial-haspopup="true"
+                arial-expanded="false"
+              >
+                <span>Hi, {  user.first_name ? user.first_name :  user.email }</span> {" "}
+              </a>
+              <div className="dropdown-menu" arial-labelledby="dropDownMenuButton">
+                <Link href="/employer/jobs" className="dropdown-item">
+                    My Jobs
+                </Link>
+                <Link href="/me/applied" className="dropdown-item">
+                    Jobs applied
+                </Link>
+                <Link href="/me/profile" className="dropdown-item">
+                    Profile
+                </Link>
+                <Link href="/upload/resume" className="dropdown-item">
+                    Upload Resume
+                </Link>
+                <Link href="/logout" className="dropdown-item text-danger">
+                    Logout
+                </Link>
+              </div>
+
+            </div>
+          ) : (
+              !loading && (
+                <Link href="/login">
+                  <button className="loginButtonHeader">
+                    <span>Login</span>
+                  </button>
+                </Link>
+              )
+          )}
         </div>
       </div>
     </div>
