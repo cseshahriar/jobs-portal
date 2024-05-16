7db9141488d59a100755a111ccc7910b2655fed0
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-05-16 07:53:13 +0600

    protecting route

--- frontend/pages/me/index.js
+++ frontend/pages/me/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import Layout from '../../components/layout/Layout'
 import UpdateProfile from '../../components/user/UpdateProfile'
+import { isAuthenticatedUser } from '../../utils/isAuthenticated'
+import { redirect } from 'next/dist/server/api-utils'
 
 export default function LoginPage() {
   return (
@@ -8,4 +10,20 @@ export default function LoginPage() {
       <UpdateProfile />
     </Layout>
   )
+}
+
+export async function getServerSideProps({ req }) {
+  const access_token = req.cookies.access
+  const user = await isAuthenticatedUser(access_token);
+  if(!user) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  } 
+  return {
+    props: { access_token}
+  }
 }
