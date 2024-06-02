546054604126c9bfa0a62a209d5385db114d9e23
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-06-02 07:16:01 +0600

    user profile admin register

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import UserProfile
 
-# Register your models here.
+
+# Define an inline admin descriptor for UserProfile model
+# which acts a bit like a singleton
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'user profiles'
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserProfileInline,)
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
